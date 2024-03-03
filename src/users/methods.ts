054708d105c3490c4b2f7ef9015f5095ad877417
RomaAlekhin <raipon@yandex.ru>
2024-03-03 19:23:52 +0100

    feat: change id type

--- src/users/methods.ts
+++ src/users/methods.ts
@@ -1,5 +1,5 @@
 import { Context } from "koa";
-import { delay } from "../utils";
+import { delay, getById } from "../utils";
 import users from "./db.json";
 
 export const getUsers = async (ctx: Context) => {
@@ -12,7 +12,9 @@ export const getUser = async (ctx: Context) => {
   await delay();
 
   const id = ctx.params.id;
-  const user = users.find((user) => user.id === Number(id));
+  const user = getById(users, id);
+
   if (!user) ctx.throw(404, `user ${id} not found`);
+
   ctx.body = user;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,12 @@
+import { WithId } from "./types";
+
 export const delay = async (delay = 0) => {
   await new Promise((resolve) => setTimeout(resolve, delay));
+};
+
+export const getById = <T extends object>(
+  data: WithId<T>[],
+  id: string
+): WithId<T> | undefined => {
+  return data.find((item) => item.id === id);
 };
