3ed5080da21cdce0b178bc22ec2a237538adaf6f
RomaAlekhin <raipon@yandex.ru>
2024-03-03 19:27:34 +0100

    fix: car route path

--- src/cars/index.ts
+++ src/cars/index.ts
@@ -2,6 +2,6 @@ import { getCar, getCars } from "./methods";
 import { router } from "../router";
 
 export const addCars = () => {
-  router.get("/cars", getCars).get("/user/:id", getCar);
+  router.get("/cars", getCars).get("/car/:id", getCar);
   return router;
 };
